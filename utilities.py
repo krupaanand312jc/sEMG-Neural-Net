92b249361aa1888813d43ee5047003358684bb1c
krupaanand312jc <mkar312jc@gmail.com>
2021-07-28 18:02:25 +0530

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -3,10 +3,10 @@ import numpy as np
 import os
 
 
-def read_data(X, data_path, split_type="train", shuffle=False, sub_split=False):
+def read_data(X, split_type="train", shuffle=False, sub_split=False):
     """
     Read data in from CSV files and format properly for neural networks.
-    :param data_path: Absolute file path to data.
+    :param X: Dataset
     :param split_type: If splitting the same dataset, which split to designate this one as.
     :param shuffle: Whether data should be kept in sequential order or shuffled.
     :param sub_split: Should the data be split in half and returned as a two-tuple.
@@ -14,6 +14,7 @@ def read_data(X, data_path, split_type="train", shuffle=False, sub_split=False):
     """
     # Fixed params
     n_class = 6
+    n_channels = 1
     n_steps = 2496
 
     train_days = [1, 2, 3]
@@ -38,70 +39,36 @@ def read_data(X, data_path, split_type="train", shuffle=False, sub_split=False):
             [[class_id for _ in range(100 * len(split))] for class_id in range(1, n_class + 1)]
         )
     )
-    # Uncomment this for the EMG Signal data from the muscle 'Flexi Carpi Ulnaris'
-    # files = [
-    #     'cyl_ch1.csv',
-    #     'hook_ch1.csv',
-    #     'lat_ch1.csv',
-    #     'palm_ch1.csv',
-    #     'spher_ch1.csv',
-    #     'tip_ch1.csv'
-    #     ]
-
-    # Uncomment this for the EMG Signal data from the muscle 'Extensor Carpi Radialis'
-    files = [
-        'cyl_ch2.csv',
-        'hook_ch2.csv',
-        'lat_ch2.csv',
-        'palm_ch2.csv',
-        'spher_ch2.csv',
-        'tip_ch2.csv'
-        ]
     
-
-    # all_of_channel = []
-    #     for file in files[num_channel::n_channels]:
-
-    #         gesture_by_day = []
-    #         for day in split:
-    #             full_day_path = os.path.join(data_path, 'male_day_%d' % day)
-    #             full_file_path = os.path.join(full_day_path, file)
-
-    #             # Drop last 4 data points to more easily subdivide into layers
-    #             gesture_by_day.append(pd.read_csv(full_file_path,  header=None).drop(labels=[2496, 2497, 2498, 2499], axis=1))
-
-    #         all_of_channel.append(pd.concat(gesture_by_day))
-    
-    
-
-
 
     if shuffle:
-        shuff_labels = np.zeros((len(labels), 1))
-        shuff_labels[:, 0] = labels
+        shuff_labels = np.zeros((len(labels), 1, n_channels))
+        shuff_labels[:, 0, 0] = labels
 
         new_data = np.concatenate([shuff_labels, X], axis=1)
 
-        np.reshape(new_data, (n_steps + 1, len(labels)))
+        np.reshape(new_data, (n_steps + 1, len(labels), n_channels))
         np.random.shuffle(new_data)
-        np.reshape(new_data, (len(labels), n_steps + 1))
+        np.reshape(new_data, (len(labels), n_steps + 1, n_channels))
 
-        final_data = new_data[:, 1:]
-        final_labels = np.array(new_data[:, 0]).astype(int)
+        final_data = new_data[:, 1:, :]
+        final_labels = np.array(new_data[:, 0, 0]).astype(int)
 
         # Return (train, test)
         if sub_split:
             return (
-                final_data[int(len(final_labels) / 2):, :],
+                final_data[int(len(final_labels) / 2):, :, :],
                 final_labels[int(len(final_labels) / 2):],
-                final_data[:int(len(final_labels) / 2), :],
+                list_of_channels,
+                final_data[:int(len(final_labels) / 2), :, :],
                 final_labels[:int(len(final_labels) / 2)],
+                list_of_channels
             )
         else:
-            return final_data, final_labels
+            return final_data, final_labels, list_of_channels
 
     else:
-        return X, Labels
+        return X, labels, list_of_channels
 
 
 def standardize(train, test):
@@ -113,8 +80,8 @@ def standardize(train, test):
     """
 
     # Standardize train and test
-    X_train = (train - np.mean(train, axis=0)[None, :]) / np.std(train, axis=0)[None, :]
-    X_test = (test - np.mean(test, axis=0)[None, :]) / np.std(test, axis=0)[None, :]
+    X_train = (train - np.mean(train, axis=0)[None, :, :]) / np.std(train, axis=0)[None, :, :]
+    X_test = (test - np.mean(test, axis=0)[None, :, :]) / np.std(test, axis=0)[None, :, :]
 
     return X_train, X_test
 
